babcd237dd5cc5890ecb1a0a77772b006571c79e
Adetola <adetolaakere1@gmail.com>
2021-03-16 21:23:58 +0100

    modify notifications

--- src/component/dashboard/TopHeader.js
+++ src/component/dashboard/TopHeader.js
@@ -30,6 +30,41 @@ function TopHeader({ setShow, show }) {
     setShow(!show);
   };
 
+  //----TIME DIFFERENCE FOR DATE----
+function timeDifference(current, previous) {
+  const milliSecondsPerMinute = 60 * 1000;
+  const milliSecondsPerHour = milliSecondsPerMinute * 60;
+  const milliSecondsPerDay = milliSecondsPerHour * 24;
+  const milliSecondsPerMonth = milliSecondsPerDay * 30;
+  const milliSecondsPerYear = milliSecondsPerDay * 365;
+
+  const elapsed = current - previous;
+
+  if (elapsed < milliSecondsPerMinute / 3) {
+    return "just now";
+  }
+
+  if (elapsed < milliSecondsPerMinute) {
+    return "less than 1 min ago";
+  } else if (elapsed < milliSecondsPerHour) {
+    return Math.round(elapsed / milliSecondsPerMinute) + " min ago";
+  } else if (elapsed < milliSecondsPerDay) {
+    return Math.round(elapsed / milliSecondsPerHour) + " h ago";
+  } else if (elapsed < milliSecondsPerMonth) {
+    return Math.round(elapsed / milliSecondsPerDay) + " days ago";
+  } else if (elapsed < milliSecondsPerYear) {
+    return Math.round(elapsed / milliSecondsPerMonth) + " month ago";
+  } else {
+    return Math.round(elapsed / milliSecondsPerYear) + " years ago";
+  }
+}
+
+   function timeDifferenceForDate(date) {
+    const now = new Date().getTime();
+    const updated = new Date(date).getTime();
+    return timeDifference(now, updated);
+  }
+
   useEffect(() => {
     dispatch(getUserMessages(user?.email));
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -66,10 +101,12 @@ function TopHeader({ setShow, show }) {
             />
             {!open && messages?.length > 0 ? (
               <div className="messages">
+              <p className="new-msg">{messages.length} New {messages.length > 1 ? 'Notifications' : 'Notification'}</p>
                 {messages.map((message) => (
                   <div key={message.id} className="message">
                     <h5>{message.subject}</h5>
                     <p>{message.message}</p>
+                   <div style={{display:'flex'}}> <span style={{fontSize:'13px'}}>{timeDifferenceForDate(message.date.split(" ")[0])}</span>
                     <button
                       onClick={() => {
                         dispatch(deleteMessage(message.id));
@@ -78,7 +115,7 @@ function TopHeader({ setShow, show }) {
                       className={font === message.id ? "read" : ""}
                     >
                       mark as read
-                    </button>
+                    </button></div>
                   </div>
                 ))}
               </div>
@@ -207,14 +244,22 @@ const HeadContainer = styled.div`
       position: absolute;
       right: 30%;
       background: #ffffff;
-      max-height: 230px;
+      max-height: 500px;
       overflow-y: scroll;
-      padding: 0.5rem 1rem;
+      /* padding: 0.5rem 1rem; */
       color: #707070;
-      width: 400px;
+      width: 300px;
       text-align: left;
       border-radius: 5px;
       box-shadow: 0px 0px 10px #00000029;
+        .new-msg{
+          margin:10px !important;
+          font-family:segoebold;
+          font-size:14px
+        }
+      p{
+        text-align:center
+      }
       @media (max-width: 400px) {
         left: 10%;
         width: 250px;
@@ -224,7 +269,27 @@ const HeadContainer = styled.div`
         left: 10%;
       }
       .message {
+        display:flex;
+        flex-direction:column;
+        align-items:flex-start;
         padding-bottom: 1.2rem;
+        border-top:1px solid #Bfc2c7;
+        padding:10px 25px;
+        /* margin-top:10px !important; */
+        h5{
+          margin:0 !important;
+          font-weight:bold;
+          font-family:segoebold !important;
+        }
+        p{
+          margin-top:5px !important;
+          text-align:left;
+          width:100%;
+          font-size:14px;
+          color:#707070;
+          font-weight:400;
+          line-height:22px
+        }
         button {
           float: right;
           background: transparent;
@@ -233,14 +298,12 @@ const HeadContainer = styled.div`
           text-transform: capitalize;
           cursor: pointer;
           outline: none;
+          margin-left:10px;
           &.read {
             font-weight: bolder;
           }
         }
-        /* h5 {
-          border-bottom: 1px solid #707070;
-          padding-bottom: 1rem;
-        } */
+
         p {
           letter-spacing: 0.32px;
           opacity: 1;
